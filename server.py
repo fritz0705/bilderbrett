a0c858990e19fbd207ed7a457970d746d804fc2d
Fritz Grimpen <fritz@grimpen.net>
2012-03-17 16:21:47 +0000

    Fixed server.py

--- server.py
+++ server.py
@@ -2,8 +2,10 @@
 # coding: utf-8
 
 from bilderbrett import app
-import tornado.ioloop
+import tornado.ioloop, tornado.httpserver, tornado.httpserver
 
 if __name__ == '__main__':
-	app.listen(65002)
+	sock = tornado.netutil.bind_unix_socket("./bilderbrett.sock")
+	server = tornado.httpserver.HTTPServer(app)
+	server.add_sockets([sock])
 	tornado.ioloop.IOLoop.instance().start()
